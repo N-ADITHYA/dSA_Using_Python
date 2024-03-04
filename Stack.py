8dc484b2b6f09f7e0d1f0b4f3a00d64a0366c430
Adithya N <nadithya666@gmail.com>
2024-03-04 20:29:43 +0530

    My first commit of Stack data structure using Python

---
+++ Stack.py
@@ -0,0 +1,33 @@
+# Understanding and Implementing the Stack Data Structure using Python
+
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self,item):
+        return self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        try:
+            return self.items[-1]
+        except Exception:
+            return Exception
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == "__main__":
+    pass
+
+
+
+
